066d588fec51fac220672a6cf30662c1a0f733c0
Mirpri <71537369+Mirpri@users.noreply.github.com>
2025-10-30 17:54:11 +0800

    fix base path

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: '/mirpri-notes/',
 
   lang: "en-US",
   title: "Mirpri's Notes",
